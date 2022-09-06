af5404247e50924f2f035c0eabeaafb69b2c7267
DerPizzaBursche <nick.roelfs@t-online.de>
2022-09-06 19:08:01 +0200

    rework

---
+++ MAUIGameLauncher/gulpfile.js
@@ -0,0 +1,11 @@
+﻿
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('styles', () => {
+    return gulp.src('wwwroot/scss/StyleSheet.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('wwwroot/css'));
+}
+gulp.task('default',gulp.series(['styles'])));
+
